f7e936e2600ff399740396033a3b2e0fe7e4c1f0
Amangeldy Shalginbayev <a.shalginbayev@yandex.com>
2022-01-19 02:23:37 +0600

    Chapter 14

---
+++ Chapter_14/DefaultAppDomainApp/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.IO;
+using System.Runtime.Loader;
+
+namespace DefaultAppDomainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with the default AppDomain *****");
+            //DisplayDADStats();
+            //ListAllAssembliesInAppDomain();
+            //LoadAdditionalAssembliesDifferentContexts();
+            LoadAdditionalAssembliesSameContext();
+            
+            Console.ReadLine();
+        }
+
+        static void DisplayDADStats()
+        {
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+            Console.WriteLine($"Name of this domain: {defaultAD.FriendlyName}");
+            Console.WriteLine($"Id of domain in this process: {defaultAD.Id}");
+            Console.WriteLine($"Is this the default domain?: {defaultAD.IsDefaultAppDomain()}");
+            Console.WriteLine($"Base directory of this domain: {defaultAD.BaseDirectory}");
+            Console.WriteLine("Setup information for this domain:");
+            Console.WriteLine($"\t Application Base: {defaultAD.SetupInformation.ApplicationBase}");
+            Console.WriteLine($"\t Target Framework: {defaultAD.SetupInformation.TargetFrameworkName}");
+        }
+
+        static void ListAllAssembliesInAppDomain()
+        {
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+
+            var loadedAssemblies = defaultAD.GetAssemblies().OrderBy(x => x.GetName().Name);
+            Console.WriteLine($"***** Here are the assemblies loaded in {defaultAD.FriendlyName} *****\n");
+            foreach (var a in loadedAssemblies)
+            {
+                Console.WriteLine($"-> Name: {a.GetName().Name}, Version: {a.GetName().Version}");
+            }
+        }
+
+        static void LoadAdditionalAssembliesDifferentContexts()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ClassLibrary1.dll");
+            AssemblyLoadContext lc1 = new AssemblyLoadContext("NewContext1", false);
+            var cl1 = lc1.LoadFromAssemblyPath(path);
+            var c1 = cl1.CreateInstance("ClassLibrary1.Car");
+
+            AssemblyLoadContext lc2 = new AssemblyLoadContext("NewContext2", false);
+            var cl2 = lc2.LoadFromAssemblyPath(path);
+            var c2 = cl2.CreateInstance("ClassLibrary1.Car");
+            Console.WriteLine("*** Loading Additional Assemblies in Different Contexts ***");
+            Console.WriteLine($"Assembly1.Equals(Assembly2) {cl1.Equals(cl2)}");
+            Console.WriteLine($"Assembly1 == Assembly2 {cl1 == cl2}");
+            Console.WriteLine($"Class1.Equals(Class2) {c1.Equals(c2)}");
+            Console.WriteLine($"Class1 == Class2 {c1 == c2}");
+        }
+
+        static void LoadAdditionalAssembliesSameContext()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ClassLibrary1.dll");
+            AssemblyLoadContext lc1 = new AssemblyLoadContext(null, false);
+            var cl1 = lc1.LoadFromAssemblyPath(path);
+            var c1 = cl1.CreateInstance("ClassLibrary1.Car");
+            var cl2 = lc1.LoadFromAssemblyPath(path);
+            var c2 = cl2.CreateInstance("ClassLibrary1.Car");
+            Console.WriteLine("*** Loading Additional Assemblies in Same Context ***");
+            Console.WriteLine($"Assembly1.Equals(Assembly2) {cl1.Equals(cl2)}");
+            Console.WriteLine($"Assembly1 == Assembly2 {cl1 == cl2}");
+            Console.WriteLine($"Class1.Equals(Class2) {c1.Equals(c2)}");
+            Console.WriteLine($"Class1 == Class2 {c1 == c2}");
+        }
+        
+        
+    }
+}
---
+++ Chapter_14/ProcessManipulator/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ProcessManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with processes *****");
+            //ListAllRunningProcesses();
+            //GetSpecificProcess();
+            //LookForThreadsByPid();
+            //EnumModsForPid(3424);
+            StartAndKillProcess();
+
+            Console.ReadLine();
+        }
+
+        static void ListAllRunningProcesses()
+        {
+            var runningProcesses = from proc in Process.GetProcesses(".")
+                orderby proc.Id
+                select proc;
+
+            foreach (var p in runningProcesses)
+            {
+                Console.WriteLine($"-> PID: {p.Id}, Name: {p.ProcessName}");
+            }
+
+            Console.WriteLine("**********\n");
+        }
+
+        static void GetSpecificProcess()
+        {
+            Process process = null;
+            try
+            {
+                process = Process.GetProcessById(816);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void EnumThreadsForPid(int pID)
+        {
+            Process p = null;
+            try
+            {
+                p = Process.GetProcessById(pID);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.WriteLine($"Here are the threads used by: {p.ProcessName}");
+
+            try
+            {
+                ProcessThreadCollection threadCollection = p.Threads;
+                foreach (ProcessThread thread in threadCollection)
+                {
+                    Console.WriteLine(
+                        $"-> Thread ID: {thread.Id}\tStart Time: {thread.StartTime.ToShortTimeString()}\tPriority:{thread.PriorityLevel}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("*******************************************\n");
+        }
+
+        static void LookForThreadsByPid()
+        {
+            Console.WriteLine("***** Enter PID of process to investigate *****");
+            Console.Write("PID: ");
+            string pID = Console.ReadLine();
+            int theProcID = int.Parse(pID);
+
+            EnumThreadsForPid(theProcID);
+        }
+
+        static void EnumModsForPid(int pID)
+        {
+            Process theProc = null;
+            try
+            {
+                theProc = Process.GetProcessById(pID);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.WriteLine($"Here are the loaded modules for: {theProc.ProcessName}");
+            ProcessModuleCollection theModules = theProc.Modules;
+            foreach (ProcessModule pm in theModules)
+            {
+                Console.WriteLine($"-> Module Name: {pm.ModuleName}");
+            }
+
+            Console.WriteLine("*************************************\n");
+        }
+
+        static void StartAndKillProcess()
+        {
+            Process process = null;
+
+            try
+            {
+                //process = Process.Start("/System/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal");
+                ProcessStartInfo startInfo = new ProcessStartInfo("/System/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal");
+                startInfo.UseShellExecute = true;
+                process = Process.Start(startInfo);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine($"--> Hit enter to kill {process.ProcessName}");
+            Console.ReadLine();
+
+            try
+            {
+                foreach (var p in Process.GetProcessesByName("Terminal"))
+                {
+                    p.Kill(true);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
